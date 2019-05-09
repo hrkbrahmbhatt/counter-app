74a998e40ddba8a6aae3272530bd517ca0850d13
hrk <hrkbrahmbhatt@gmail.com>
2019-05-09 19:20:47 -0400

    destructing arugments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 
-const Navbar = (props) => {
+const Navbar = ({totalCounters}) => {
   return ( 
 
     <nav className="navbar navbar-light bg-light">
-<a className="navbar-brand" href="#">Navbar <span className="badge badge-pill badge-seconday">{props.totalCounters}</span></a>
+<a className="navbar-brand" href="#">Navbar <span className="badge badge-pill badge-seconday">{totalCounters}</span></a>
 </nav>
 
    );
