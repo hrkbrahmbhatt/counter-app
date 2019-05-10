065f963e1af72dacedfa30770fcce3c21aebdd9d
hrk <hrkbrahmbhatt@gmail.com>
2019-05-09 20:09:51 -0400

    added Decrement button  and grid view

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,12 +4,12 @@ class Counters extends Component {
   
   render() { 
 
-      const {onReset, onIncrement, onDelete, counters} = this.props;
+      const {onReset, onIncrement, onDelete, counters, onDecrement} = this.props;
     return (  
       
       <div>
         <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
-        {counters.map(counter => <Counter key={counter.id} onDelete={onDelete} onIncrement={onIncrement} counter={counter}/>)}
+        {counters.map(counter => <Counter key={counter.id} onDelete={onDelete} onIncrement={onIncrement} onDecrement={onDecrement} counter={counter}/>)}
       </div>
     );
   }
